d8119e86ebee2a381d67b2d853a7c22503d94846
Darthwimp <debargha2003.das@gmail.com>
2022-11-09 15:58:32 +0530

    info array

---
+++ lib/info.dart
@@ -0,0 +1,37 @@
+const info = [
+  {
+    "name": "Never",
+    "message": "Hey There! Are you using whatsapp?",
+    "time": "8:58",
+    "type": "new",
+    "mode": "recieved",
+  },
+  {
+    "name": "Gonna",
+    "message": "What are you doing tomorrow?",
+    "time": "8:58",
+    "type": "new",
+    "mode": "recieved",
+  },
+  {
+    "name": "Give",
+    "message": "How is your scholarship going on?",
+    "time": "8:58",
+    "type": "old",
+    "mode": "sent",
+  },
+  {
+    "name": "You",
+    "message": "When are the elections?",
+    "time": "8:58",
+    "type": "new",
+    "mode": "recieved",
+  },
+  {
+    "name": "Up",
+    "message": "Yo whassup!",
+    "time": "8:58",
+    "type": "old",
+    "mode": "sent",
+  },
+];
